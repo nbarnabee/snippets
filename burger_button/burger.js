1b2d40a7b5bc005f744e352340dbc60023a4c1e0
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-03-07 14:22:19 +0100

    Reorganize files

--- burger_button/burger.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".dropmenu"),
-  navmenu = document.querySelector(".menu");
-menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("open");
-    navmenu.classList.toggle("open");
-  } );

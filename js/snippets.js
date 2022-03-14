101e56b9ff8559eda49ad003e475c3470dbb4c61
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-03-14 11:21:50 +0100

    Add one snippet & fix another

--- js/snippets.js
+++ js/snippets.js
@@ -13,11 +13,11 @@ function FUNCTIONNAME(click) {
 // Pick a random element from an array
 
 function randomElement(array) {
-  //generate a number between 0 and the length of the array
-  let randomNum = Math.floor(Math.random() * array.length); 
+  //generate a number between 0 and the length of the array - 1 (because the array's length will always be one greater than the index position of its final element)
+  let randomNum = Math.floor(Math.random() * (array.length - 1)); 
   console.log(`${randomNum}`);
-  let randomArray = array[randomNum];
-  return randomArray;
+  let randomArrayElement = array[randomNum];
+  return randomArrayElement;
 };
 
 
@@ -26,3 +26,21 @@ function randomElement(array) {
 // document.documentElement represents the :root element
 const rootStyles = getComputedStyle(document.documentElement);
 const scale3 = rootStyles.getPropertyValue("--s3");
+
+
+
+//comparing the values of two arrays and placing the shared values into a third array
+const bobsFollowers = ["Steve", "Eric", "Hans", "Patrick"];
+
+const tinasFollowers = ["Jessica", "Patrick","Steve"];
+
+const mutualFollowers = [];
+
+for (let i = 0; i<bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(tinasFollowers[j]);
+    };
+  };
+}
+console.log(mutualFollowers);

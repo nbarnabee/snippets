ce02573333e6043ca7f5ae38d276677b570cfd2a
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-05-13 16:51:23 +0200

    Initial commit

---
+++ js/helper_functions.js
@@ -0,0 +1,47 @@
+// Pick a random element from an array
+
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)]; 
+};
+
+
+//comparing the values of two arrays and place the shared values into a third array
+
+function filterShared(arr1, arr2) {
+  let shared = arr1.filter(a => arr2.includes(a));
+  return shared;
+}
+
+// comparing the values of two arrays and seeing whether all of the values in the first array are contained in the section
+
+const containsAll = (arr1, arr2) => arr1.every(ele => arr2.includes(ele));
+
+
+// related, if you're trying to match a set of values to one of a number of sets contained within a 2D array (e.g., in my tic-tac-toe game)
+// however, this only works if the two sets are EXACT matches, with no extra numbers
+
+function evaluateSet(set, twoDArray) {
+  for (i = 0; i < twoDArray.length; i++) {
+    if (set.every(ele => twoDArray[i].includes(ele)))
+    return true
+  }
+}
+
+// this function is what I ended up using with my tic-tac-toe game, to see if all of the numbers from a particular array within the array of arrays could be found in the target array
+
+function evaluateSetInsideOut(set, twoDArray) {
+  for (i = 0; i < twoDArray.length; i++) {
+    if (twoDArray[i].every(ele => set.includes(ele)))
+    return true
+  }
+}
+
+// to go deeper with multidimensional arrays, I would just need to add nested for loops
+
+
+// counting occurrences of a particular value in an array
+
+const occurrenceCount = arr => arr.reduce((count, value) => {
+  count[value] = count[value] ? count[value] + 1 : 1;
+  return count;
+}, {});

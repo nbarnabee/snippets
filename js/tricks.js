ea4d2023d6cf01880cff27b3ab8ce0e2b82532dc
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-10-12 10:10:05 +0200

    Add new code snippet and explanation

--- js/tricks.js
+++ js/tricks.js
@@ -29,3 +29,30 @@ function twoSum(arr, target) {
     }
   }
 }
+
+// Find the position of a value in a matrix
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+// To find the "row" - i.e., the index of the subarray that contains the sought-after value..
+
+function findRow(arr, value) {
+  return arr.findIndex((row) => row.indexOf(value) !== -1);
+}
+
+// To find both column and row  - i.e., the index of the subarray that contains the value AND the index position of the value within that subarray..
+
+function find2d(arr, value) {
+  let row = 0,
+    col = -1;
+  while (row < arr.length && (col = arr[row].indexOf(value)) === -1) row++;
+  // here we have a loop that will iterate over the rows, so long as the value does not appear in the given row (in other words, the indexOf === -1)
+  return row === arr.length ? "Value not found" : [row, col];
+  // if we reach a point where we've gone past the number of elements in the matrix (which would mean that the item has never been found)
+  // we return the error statement, or else we return the current value of row and the value of col, which was set in line 51
+  // it's important to remember that so long as the value of col remains -1 (and the row does not exceed the number of elements in the array), the while loop will keep executing.
+  // that's how this is working
+}
